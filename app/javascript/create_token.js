a9830a6e710aa01b24cc3d74739704f6146d6725
Akinori Sugiyama <49008705+Akinori0123@users.noreply.github.com>
2023-01-05 17:39:00 +0900

    console.logの削除

--- app/javascript/create_token.js
+++ app/javascript/create_token.js
@@ -16,7 +16,6 @@ const pay = ()=> {
     payjp.createToken(numberElement).then(function (response) {
       if (response.status === 402) {
       } else {
-        console.log(response)
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} type="hidden" name='pay_form[token]'>`;
